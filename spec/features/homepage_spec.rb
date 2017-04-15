0ea6177535c825a034187736432a1817a638009b
Sebastian Nepote <sebastian.nepote@helpling.com>
2017-04-15 19:34:45 +0200

    homepage template with tests

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 feature 'Homepage feature' do
   scenario 'As a user I want to see a text in the homepage', type: :feature do
       visit '/'
-      expect(page).to have_content('some text')
+      expect(page).to have_content('StaticPages#home')
   end
 
   scenario 'As a user I want to see a text in the homepage using javascript',
   type: :feature,
   js: true do
       visit '/'
-      expect(page).to have_content('some text')
+      expect(page).to have_content('StaticPages#home')
   end
 end
